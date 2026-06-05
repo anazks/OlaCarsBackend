27b3a1d3bd57a6e84cc4a4d62df96c205d2ba905
anazks <anazksunil2@gmail.com>
2026-06-05 17:00:15 +0530

    added logout isuue

--- Src/config/jwtConfig.js
+++ Src/config/jwtConfig.js
@@ -1,6 +1,6 @@
 const jwtConfig = {
-  accessTokenExpiry: "15m",
-  refreshTokenExpiry: "7d",
+  accessTokenExpiry: "10h",
+  refreshTokenExpiry: "3650d", // 10 years to stay logged in indefinitely
 };
 
 module.exports = { jwtConfig };
